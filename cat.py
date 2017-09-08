3fcb36b747d66ab1da4f107b4e741e9a53c36424
Christopher Nguyen <cnguyen5413@gmail.com>
2017-09-07 19:36:24 -0700

    Commented cat and allowed multiple files without options

--- cat.py
+++ cat.py
@@ -16,13 +16,15 @@ def main():
     for input in sys.stdin:
       sys.stdout.write(input) 
     
-  elif(len(files) == 2):
+  elif(len(files) == 2):    #Runs if arguments has no file or only the file
     if("-n" == files[1] or files[1] == "-b"):
       for line in sys.stdin:
         line_num += 1
         if(files[1] == "-b" and len(line) == 0):
           line_num -= 1
           continue
+
+	#number_space accounts for the spaces before the number for greater accuracy
         number_space = max((6-int(len(str(line_num)))),0) * " "
         sys.stdout.write(number_space + str(line_num) + "  " + line)
       return
@@ -30,13 +32,20 @@ def main():
     else:
       start_index = 1
 
+#The first if of this elif checks whether there were options or not
   elif(len(files) >= 3):
-    start_index = 2
+    if(files[1] == "-n" or files[1] == "-b"):
+      start_index = 2
+    else:
+      start_index = 1
+ 
+
+#This for loops runs throught the entire remaining files, also checking the options
+#along the way
 
   for file in files[start_index:]:
     current_file = open(file)
     for line in current_file.readlines():
-      number_space = ""
       line_num += 1
       if("-n" == files[1] or files[1] == "-b"):
         if(files[1] == "-b" and len(line) == 0):
